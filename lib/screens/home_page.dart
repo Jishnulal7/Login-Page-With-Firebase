26705ad79a339b2a778702d20c9c42d635ccce6d
jishnulal7 <jishnulal@9497@gmail.com>
2023-05-06 01:41:43 +0530

    Update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -9,14 +9,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final user =  FirebaseAuth.instance.currentUser!;
-  
+  final user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Text("Signed In as "),
-      ]),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            "Signed In as : ${user.email}",
+            style: const TextStyle(fontSize: 20),
+          ),const SizedBox(
+            height: 10,
+          )
+          ,
+          MaterialButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            color: Colors.deepPurple,
+            child: const Text(
+              'SignOut',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
